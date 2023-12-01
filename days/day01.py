05427c7a2f5708b3ad3e63113e0994d83211abb3
Duuk Niemantsverdriet <d.p.niemantsverdriet@student.tudelft.nl>
2023-12-01 06:19:19 +0100

    Day 1 (and so it begins)

--- days/day01.py
+++ days/day01.py
@@ -10,13 +10,53 @@ def solve(data, part=2):
 
 
 def part_one(data):
-    pass
+    res = 0
+    for l in data:
+        n = ""
+        for c in l:
+            if c.isdigit():
+                n += c
+                break
+
+        for c in reversed(l):
+            if c.isdigit():
+                n += c
+                break
+
+        res += int(n)
+
+    return res
 
 
 def part_two(data):
-    pass
+    digits = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+
+    res = 0
+    for l in data:
+        n = ""
+        for c in (range(len(l))):
+            if l[c].isdigit():
+                n += l[c]
+                break
+            arr = [l[c:].startswith(d) for d in digits]
+            if any(arr):
+                n += str(arr.index(True))
+                break
+
+        for c in reversed(range(len(l))):
+            if l[c].isdigit():
+                n += l[c]
+                break
+            arr = [l[:c+1].endswith(d) for d in digits]
+            if any(arr):
+                n += str(arr.index(True))
+                break
+
+        res += int(n)
+
+    return res
 
 
 if __name__ == "__main__":
-    print(solve(load_input("small"), 1))
-    print(solve(load_input("small")))
+    print(solve(load_input(), 1))
+    print(solve(load_input()))
