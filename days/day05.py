099435a521ba105f93899b500d291ecd8189abdc
Duuk Niemantsverdriet <d.p.niemantsverdriet@student.tudelft.nl>
2023-12-05 09:11:33 +0100

    Day 5 cleanup

--- days/day05.py
+++ days/day05.py
@@ -9,23 +9,29 @@ def solve(data, part=2):
         return part_two(lines)
 
 
-def part_one(data):
+def parse_data(data):
     seeds = [int(n) for n in data[0].split(": ")[1].split(" ")]
 
-    maps = {}
+    maps = []
     current = None
     for line in data[2:]:
         if line.endswith(":"):
-            maps[line[:5]] = []
-            current = maps[line[:5]]
+            current = len(maps)
+            maps.append([])
         elif line:
-            current.append(tuple(int(n) for n in line.split(" ")))
+            maps[current].append(tuple(int(n) for n in line.split(" ")))
+
+    return seeds, maps
+
+
+def part_one(data):
+    seeds, maps = parse_data(data)
 
     finals = []
     for seed in seeds:
         curr = seed
-        for m in ["seed-", "soil-", "ferti", "water", "light", "tempe", "humid"]:
-            for dest_start, src_start, length in maps[m]:
+        for m in maps:
+            for dest_start, src_start, length in m:
                 if curr >= src_start and curr < src_start + length:
                     curr = dest_start + (curr - src_start)
                     break
@@ -35,44 +41,32 @@ def part_one(data):
 
 
 def part_two(data):
-    seeds = [int(n) for n in data[0].split(": ")[1].split(" ")]
+    seeds, maps = parse_data(data)
 
-    maps = {}
-    current = None
-    for line in data[2:]:
-        if line.endswith(":"):
-            maps[line[:5]] = []
-            current = maps[line[:5]]
-        elif line:
-            current.append(tuple(int(n) for n in line.split(" ")))
-
-    finals = []
-    for i in range(0, len(seeds), 2):
-        seed_ranges = [(seeds[i], seeds[i + 1])]
-        for m in ["seed-", "soil-", "ferti", "water", "light", "tempe", "humid"]:
-            new_ranges = []
-            while seed_ranges:
-                (s, l) = seed_ranges.pop(0)
-                for dest_start, src_start, length in maps[m]:
-                    if s >= src_start and s < src_start + length:
-                        if s + l <= src_start + length:
-                            new_ranges.append((dest_start + s - src_start, l))
-                            break
-                        else:
-                            l1 = (src_start + length) - s
-                            new_ranges.append((dest_start + s - src_start, l1))
-                            seed_ranges.append((src_start + length, l - l1))
-                            break
-                    elif s + l > src_start and s + l <= src_start + length:
-                        new_ranges.append((dest_start, s + l - src_start))
-                        seed_ranges.append((s, src_start - s))
+    seed_ranges = [(seeds[i], seeds[i + 1]) for i in range(0, len(seeds), 2)]
+    for m in maps:
+        new_ranges = []
+        while seed_ranges:
+            (s, l) = seed_ranges.pop(0)
+            for dest_start, src_start, length in m:
+                if src_start <= s < src_start + length:
+                    if s + l <= src_start + length:
+                        new_ranges.append((dest_start + s - src_start, l))
                         break
-                else:
-                    new_ranges.append((s, l))
-            seed_ranges = new_ranges
-        finals.append(min(r[0] for r in seed_ranges))
+                    else:
+                        l1 = (src_start + length) - s
+                        new_ranges.append((dest_start + s - src_start, l1))
+                        seed_ranges.append((src_start + length, l - l1))
+                        break
+                elif src_start < s + l <= src_start + length:
+                    new_ranges.append((dest_start, s + l - src_start))
+                    seed_ranges.append((s, src_start - s))
+                    break
+            else:
+                new_ranges.append((s, l))
+        seed_ranges = new_ranges
 
-    return min(finals)
+    return min(r[0] for r in seed_ranges)
 
 
 if __name__ == "__main__":
